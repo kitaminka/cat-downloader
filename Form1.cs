d29a4067eeb72931455862ce14705a7fc334b094
kitaminka <kit4minka@outlook.com>
2023-01-23 21:40:07 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cat_downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.button1.Text = "Downloading...";
+            var task = Task.Run(async () => {
+                using (WebClient client = new WebClient())
+                {
+                    for (int i = 0; i < this.numericUpDown1.Value; i++)
+                    {
+                        await Task.Delay(10000);
+                        client.DownloadFile(new Uri("https://thiscatdoesnotexist.com/"), @"cat_" + i + 1 + ".png");
+                    }
+                }
+            });
+        }
+    }
+}
